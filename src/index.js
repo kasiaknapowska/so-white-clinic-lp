711f54c3c160d67cadaa7c6778d618339811cbad
KatarzynaKnapowska <k.knapowska@gmail.com>
2023-08-21 14:53:57 +0200

    styles, thank you page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { Cookies } from "./js/cookies";
 import logo from "./assets/logo.png";
 import fb from "./assets/icons/icon-fb.svg";
 import insta from "./assets/icons/icon-insta.svg";
+import phone from "./assets/icons/icon-phone.svg";
+import home from "./assets/icons/icon-home.svg";
+import mail from "./assets/icons/icon-mail.svg";
 
 window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
@@ -15,6 +18,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
   fbImg.src = fb;
   const inImg = document.getElementById("in");
   inImg.src = insta;
+  const phoneImg = document.getElementById("phone");
+  phoneImg.src = phone;
 
   //form
   const form = document.querySelector("#contact-form");
@@ -27,11 +32,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
     "acceptpolicy",
   ];
   const loader = document.querySelector(".lds-ring");
-  const submitBtn = document.querySelector(".submit-text")
+  const submitBtn = document.querySelector(".submit-text");
   const validator = new Form(form, fields, loader, submitBtn);
 
-// cookie banner
-const cookieBanner = document.querySelector(".cookies");
-const cookies = new Cookies(cookieBanner);
-
+  // cookie banner
+  const cookieBanner = document.querySelector(".cookies");
+  const cookies = new Cookies(cookieBanner);
 });
